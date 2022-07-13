a3aec745b1adaf65ec3768c2e14eb56856e09e19
Iñaki_Alegre <iki88ma@gmail.com>
2022-07-13 18:18:32 +0200

    conexion a la bbdd firestore y primeras consultas para mostrar datos en los select de step1.html

---
+++ firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDcJVXoVRZjgyJhYtwGqc-HLig7jVYPIVM",
+    authDomain: "webmagister-ca101.firebaseapp.com",
+    projectId: "webmagister-ca101",
+    storageBucket: "webmagister-ca101.appspot.com",
+    messagingSenderId: "568316995023",
+    appId: "1:568316995023:web:464b0c477cfad5a2ac5eba",
+    measurementId: "G-0D23TRQPPF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+export const getRama = () => getDocs(collection(db, 'ramas'))
+export const getProvincia = () =>getDocs(collection(db, 'provincias'))
+
+
--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getProvincia, getRama} from './firebase.js'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDcJVXoVRZjgyJhYtwGqc-HLig7jVYPIVM",
-    authDomain: "webmagister-ca101.firebaseapp.com",
-    projectId: "webmagister-ca101",
-    storageBucket: "webmagister-ca101.appspot.com",
-    messagingSenderId: "568316995023",
-    appId: "1:568316995023:web:464b0c477cfad5a2ac5eba",
-    measurementId: "G-0D23TRQPPF"
-};
+const selectRama = document.getElementById('rama')
+window.addEventListener('DOMContentLoaded', async ()=>{
+    const querySnapshot = await getRama();
+    let html = ''
+    querySnapshot.forEach(doc => {
+        //console.log(doc.data())
+        const rama = doc.data()
+        html +=
+        `<option value="">${rama.rama1}</option>
+        <option value="">${rama.rama2}</option>`
+    
+    })
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+    selectRama.innerHTML = html
+})
+
+const selectProvincia = document.getElementById('provincia')
+window.addEventListener('DOMContentLoaded', async() =>{
+    const querySnapshot = await getProvincia();
+    let html = ''
+    querySnapshot.forEach(doc => {
+        console.log(doc.data())
+        const provincia = doc.data()
+        html +=
+        `<option value="">${provincia.Provincia1}</option>
+        <option value="">${provincia.Provincia2}</option>`
+    })
+    selectProvincia.innerHTML = html;
+})
